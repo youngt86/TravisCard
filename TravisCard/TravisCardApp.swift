cd402a40dc08280c9b94eac0bf97418bc8f625ad
Travis Young <youngtravis424@gmail.com>
2023-03-29 16:35:06 -0400

    Initial Commit

---
+++ TravisCard/TravisCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravisCardApp.swift
+//  TravisCard
+//
+//  Created by Travis Young on 3/29/23.
+//
+
+import SwiftUI
+
+@main
+struct TravisCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
